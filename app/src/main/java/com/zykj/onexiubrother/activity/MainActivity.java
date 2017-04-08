087fb993ff2b1cabf39e888341e6fa78904f1716
sun309412453 <229265203@qq.com>
2017-04-08 13:46:31 +0800

    新建主页面

---
+++ app/src/main/java/com/zykj/onexiubrother/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.zykj.onexiubrother.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.zykj.onexiubrother.R;
+
+/**
+ * Created by zykj on 2017/4/8.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
