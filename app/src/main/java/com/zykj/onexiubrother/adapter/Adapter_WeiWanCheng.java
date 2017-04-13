93371b53ad42bd88c06b28b9ceaadf4a2110adeb
sun309412453 <229265203@qq.com>
2017-04-13 16:24:05 +0800

    修改Activity页面

--- app/src/main/java/com/zykj/onexiubrother/adapter/Adapter_WeiWanCheng.java
+++ app/src/main/java/com/zykj/onexiubrother/adapter/Adapter_WeiWanCheng.java
@@ -2,9 +2,13 @@ package com.zykj.onexiubrother.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.zykj.onexiubrother.R;
 import com.zykj.onexiubrother.bean.WeiWanChengBean;
 
 import java.util.List;
@@ -17,22 +21,29 @@ public class Adapter_WeiWanCheng extends RecyclerView.Adapter<Adapter_WeiWanChen
     private List<WeiWanChengBean> list;private Context context;
     @Override
     public WeiWanChengHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.zhuangtai_weiwancheng_item,parent,false);
+        WeiWanChengHolder holder = new WeiWanChengHolder(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(WeiWanChengHolder holder, int position) {
-
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
     public class WeiWanChengHolder extends RecyclerView.ViewHolder {
+        TextView zhonglei,date,zhuangtai,add_item;Button chakan;
         public WeiWanChengHolder(View itemView) {
             super(itemView);
+            zhonglei= (TextView) itemView.findViewById(R.id.zhonglei);
+            date= (TextView) itemView.findViewById(R.id.date);
+            zhuangtai= (TextView) itemView.findViewById(R.id.zhuangtai);
+            add_item= (TextView) itemView.findViewById(R.id.add_item);
+            chakan = (Button) itemView.findViewById(R.id.chakan);
         }
     }
 }
--- app/src/main/java/com/zykj/onexiubrother/bean/WeiWanChengBean.java
+++ app/src/main/java/com/zykj/onexiubrother/bean/WeiWanChengBean.java
@@ -5,5 +5,41 @@ package com.zykj.onexiubrother.bean;
  */
 
 public class WeiWanChengBean {
-    private String ZhongLei,Address;
+    private String ZhongLei;
+    private String Address;
+
+    @Override
+    public String toString() {
+        return "WeiWanChengBean{" +
+                "ZhongLei='" + ZhongLei + '\'' +
+                ", Address='" + Address + '\'' +
+                ", Date='" + Date + '\'' +
+                '}';
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getZhongLei() {
+        return ZhongLei;
+    }
+
+    public void setZhongLei(String zhongLei) {
+        ZhongLei = zhongLei;
+    }
+
+    private String Date;
 }
