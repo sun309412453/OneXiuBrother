dbcd439d035f25babe646cac184396e5b5f3a142
sun309412453 <229265203@qq.com>
2017-04-13 12:51:24 +0800

    修改Activity页面

--- app/src/main/java/com/zykj/onexiubrother/activity/Activity_Call_Service.java
+++ app/src/main/java/com/zykj/onexiubrother/activity/Activity_Call_Service.java
@@ -1,18 +1,44 @@
 package com.zykj.onexiubrother.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.zykj.onexiubrother.R;
+import com.zykj.onexiubrother.widget.MyTitleBar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by zykj on 2017/4/13.
  */
 
 public class Activity_Call_Service extends Activity {
+    @Bind(R.id.title)
+    MyTitleBar title;
+    @Bind(R.id.dizhi)
+    LinearLayout dizhi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.call_service);
+        ButterKnife.bind(this);
+        title.setLeftClick(new MyTitleBar.leftClick() {
+            @Override
+            public void Click(View view) {
+                finish();
+            }
+        });
+    }
+
+    @OnClick(R.id.dizhi)
+    public void onViewClicked() {
+        Intent diZhiIntent = new Intent();
+        startActivity(diZhiIntent);
     }
 }
